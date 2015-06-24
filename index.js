60926a0565340d71a29e7913f9fa5f69cf32b4cd
Takeyuki Kojima <unau@users.noreply.github.com>
2015-06-25 04:12:10 +0900

    Introduce sub glace classes

--- index.js
+++ index.js
@@ -9,15 +9,15 @@
   })();
   nougat.isGas = nougat.env.name === 'GAS';
   nougat.isNode = nougat.env.name === 'Node';
-  nougat.log = function(arg) {
-    return nougat.isGas ? Logger.log(arg) : console.log(arg);
+  var Glace = function() {};
+  'env,isGas,isNode'.split(',').forEach(function(key) {
+    Glace.prototype[key] = nougat[key];
+  });
+  function getNewGlace(arg, name, body) {
+    var cnst = nougat.isNode ? Glace.Node : Glace.Gas;
+    return new cnst().initAs(arg).init(name, body);
   }
-  var Glace = function(arg, name, body) {
-    this.init(arg, name, body);
-  };
-  Glace.prototype.init = function(arg, name, body) {
-    this.global = arg.g;
-    this.module = arg.m;
+  Glace.prototype.init = function(name, body) {
     this.name = name;
     this.body = body;
     return this;
@@ -27,29 +27,80 @@
       if (typeof this.body != 'function') return this;
       stuff = this.body(this);
     }
-    if (this.isGas) {
-      if (this.name) {
-	this.global[this.name] = stuff;
-      } else {
-	for (var key in stuff) {
-	  this.global[key] = stuff[key];
-	}
-      }
-    } else {
-      this.module.exports = stuff;
-    }
+    this._export_(stuff);
     return this;
   };
-  'env,isGas,isNode,log'.split(',').forEach(function(key) {
-    Glace.prototype[key] = nougat[key];
+  (function(spec) {
+    for (var methodName in spec) {
+      var methodSpec = spec[methodName];
+      for (var subName in methodSpec) {
+        var method = methodSpec[subName];
+        if (method) {
+          var subclass = Glace[subName];
+          if (! subclass) {
+            subclass = Glace[subName] = function() {};
+            subclass.prototype = new Glace();
+          }
+          subclass.prototype[methodName] = method;
+        }
+      }
+    }
+  })({
+    initAs: {
+      Node: function(arg) {
+        this.global = arg.g;
+        this.module =  arg.m,
+        this.u = { path: require('path') };
+        this.dir = this.u.path.dirname(arg.m.id)
+        return this;
+      },
+      Gas: function(arg) {
+        this.global = arg.g;
+        this.global._nougatport = this.global._nougatport || {};
+        return this;
+      }
+    },
+    _export_: {
+      Node: function(stuff) {
+        this.module.exports = stuff;
+      },
+      Gas: function(stuff) {
+        if (this.name) {
+          this.global._nougatport[this.name] = stuff;
+        } else {
+          for (var key in stuff) {
+            this.global[key] = stuff[key];
+          }
+        }
+      }
+    },
+    require: {
+      Node: function(arg) {
+        return require(this.u.path.resolve(this.dir, arg));
+      },
+      Gas: function(arg) {
+        var key = (function(a) {
+          return a[a.length - 1];
+        })(arg.split('/'));
+        return this.global._nougatport[key];
+      }
+    },
+    slog: {
+      Node: function(arg) {
+        return console.log(arg);
+      },
+      Gas: function(arg) {
+        return Logger.log(arg);
+      }
+    }
   });
-  return new Glace(arg)._export({$: function(name, arg, body) {
+  return getNewGlace(arg)._export({$: function(name, arg, body) {
     if (! body) {
       body = arg;
       arg = name;
       name = null;
     }
-    return new Glace(arg, name, body)._export();
+    return getNewGlace(arg, name, body)._export();
   }});
 })(this.UiApp ? {g: this} : {g: global, m: module}, {});
 // end of file
